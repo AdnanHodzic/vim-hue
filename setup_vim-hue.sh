41580380baae9379652e8aabf928e084c1162811
Adnan Hodzic <adnan@hodzic.org>
2016-11-18 17:13:00 +0100

    made initial deb config

--- setup_vim-hue.sh
+++ setup_vim-hue.sh
@@ -22,30 +22,34 @@ then
 fi
 }
 
-# make sure vim is installed
+# check if vim is installed/if not install?
 #
 # if 74 vs 8 different location completely
-#
-# make config backups
 
-# ubuntu/debian 74
-# /usr/share/vim/vim74/colors
-#
-# config:
-# /etc/vim/vimrc.local
-#
-#
-# centos/redhat 74
-# /usr/share/vim/vim74/colors
-#
-# config:
-# /etc/vimrc
-#
-# mac
-# /usr/share/vim/vim72/colors
-#
-# config:
-# /usr/share/vim/vimrc
+# debian/ubuntu vim74
+deb_vim74="/usr/share/vim/vim74/colors"
+deb_vim_conf="/etc/vim/vimrc.local"
+deb_vim_conf_bak="/etc/vim/vimrc.local.org.bak"
+
+# conf backup
+if [ -f $deb_vim_conf ]
+then
+	echo "Found existing: $deb_vim_conf
+	echo "Backup location: $deb_vim_conf_bak
+	cp -f $deb_vim_conf $deb_vim_conf_bak
+	echo ""
+fi
+
+# vim-hue conf
+cp ./hue.vim $deb_vim74
+
+# redhat/centos vim74
+rh_vim74="/usr/share/vim/vim74/colors"
+rh_vim_conf="/etc/vim/vimrc.local"
+
+# mac vim74
+mac_vim74="/usr/share/vim/vim74/colors"
+mac_vim_conf="/usr/share/vim/vimrc"
 
 # hue install:
 #
