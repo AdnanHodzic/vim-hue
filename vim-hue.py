9d38ce9bf01f87fc3545ca66a0749aa8d8a28ee9
Adnan Hodzic <adnan@hodzic.org>
2019-02-20 23:17:48 +0100

    added global_vim_conf for system wide vim configuration

--- vim-hue.py
+++ vim-hue.py
@@ -8,23 +8,17 @@ from distutils.dir_util import copy_tree
 # global vars
 userhome = os.path.expanduser('~')
 vim = userhome + "/.vim"
-vimBakDir = vim + ".backup"
-hueSrcDir = os.getcwd()
 vimConfDir = vim
+globalVim = "/etc/vim"
+globalVimConfDir = globalVim
+vimBakDir = vim + ".backup"
+globalVimBakDir = globalVim + ".backup"
+hueSrcDir = os.getcwd()
 
 # dir vim (conf) dir func
-def create_vim_dir(target_dir):
+def create_dir(target_dir):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-    elif os.path.exists(target_dir):
-        # necessary for overwrite if vimBakDir exists
-        if os.path.exists(vimBakDir):
-            shutil.rmtree(vimBakDir)
-            print("Found and backed up existing Vim configuration:\n" + vimBakDir +  "\n")
-            shutil.move(vim, vimBakDir)
-        else:
-            print("Found and backed up existing Vim configuration:\n" + vimBakDir)
-            shutil.move(vim, vimBakDir)
 
 def vim_conf():
     # ToDo:
@@ -32,11 +26,42 @@ def vim_conf():
     # - create necessary ops + dir structure for global vimConfDir
     # - for global config make root check
     print("\n" + 20 * "-" + " vim-hue " + 20 * "-" + "\n")
-    create_vim_dir(vim)
-    print("New Vim (vim-hue) configuration stored in:\n" + vim)
+
+    create_dir(vim)
+    # necessary for overwrite if vimBakDir exists
+    if os.path.exists(vimBakDir):
+        shutil.rmtree(vimBakDir)
+        print("Found and backed up existing Vim configuration:\n" + vimBakDir +  "\n")
+        shutil.move(vim, vimBakDir)
+    else:
+        print("Found and backed up existing Vim configuration:\n" + vimBakDir)
+        shutil.move(vim, vimBakDir)
+
+    print("\nNew Vim (vim-hue) configuration stored in:\n" + vim)
     print("\n" + 49 * "-" + "\n")
+
     # populate vimConfDir with hueSrcDir
     copy_tree(hueSrcDir, vimConfDir)
+
+def global_vim_conf():
+    print("\n" + 20 * "-" + " vim-hue " + 20 * "-" + "\n")
+    #root check
+
+    create_dir(globalVim)
+    # necessary for overwrite if globalVimBakDir exists
+    if os.path.exists(globalVimBakDir):
+        shutil.rmtree(globalVimBakDir)
+        print("Found and backed up existing Vim configuration:\n" + globalVimBakDir +  "\n")
+        shutil.move(globalVim, globalVimBakDir)
+    else:
+        print("Found and backed up existing Vim configuration:\n" + globalVimBakDir)
+        shutil.move(globalVim, globalVimBakDir)
+
+    print("\nNew Vim (vim-hue) configuration stored in:\n" + globalVim)
+    print("\n" + 49 * "-" + "\n")
+
+    # populate vimConfDir with hueSrcDir
+    copy_tree(hueSrcDir, globalVimConfDir)
 
 def vim_ver_check():
     getVimVer = subprocess.getoutput("vim --version | grep \"VIM - Vi IMproved\" | cut -d\" \" -f5 | grep -oE \"^\s*[0-9]+\"")
@@ -46,5 +71,8 @@ def vim_ver_check():
         print("Only Vim version >= 8 are supported")
         quit()
 
-#vim_conf()
 vim_ver_check()
+# ToDo:
+# Add to options menu
+#vim_conf()
+#global_vim_conf()
